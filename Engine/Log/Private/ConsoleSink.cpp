e021f6cc5b9e46f62c6c6772a70d86b76d5a3f5d
pulnip <choiwh1008@gmail.com>
2026-02-13 02:38:38 +0900

    change logging stream

--- Engine/Log/Private/ConsoleSink.cpp
+++ Engine/Log/Private/ConsoleSink.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 #include "ConsoleSink.hpp"
 
 namespace Crowy
@@ -8,7 +7,7 @@ namespace Crowy
         std::tm tm;
         auto tp = std::chrono::system_clock::to_time_t(msg.time_point);
 
-        auto& out = msg.level >= LogLevel::Error ? std::cerr : std::cout;
+        auto& out = msg.level >= LogLevel::Error ? std::cerr : std::clog;
 
         std::println(out, "[{:%H:%M:%S}][{}][{}]: {}({}:{})",
             msg.time_point, levelToString(msg.level), msg.category.name,
